baf4db7040a903357e31a578e7418c04c5421f0d
lz <lz@lz-PC>
2015-06-18 11:15:14 +0800

    fix bug

--- VolleyDemo/src/com/example/volleydemo/VolleyBitmapLruCache.java
+++ VolleyDemo/src/com/example/volleydemo/VolleyBitmapLruCache.java
@@ -1,11 +1,13 @@
 package com.example.volleydemo;
 
+import android.annotation.SuppressLint;
 import android.content.Context;
 import android.graphics.Bitmap;
-import android.support.v4.util.LruCache;
+import android.util.LruCache;
 
 import com.android.volley.toolbox.ImageLoader.ImageCache;
 
+@SuppressLint("NewApi")
 class VolleyBitmapLruCache extends LruCache<String, Bitmap> implements ImageCache {  //采用Lru算法计算缓存
 
 	
@@ -19,15 +21,18 @@ class VolleyBitmapLruCache extends LruCache<String, Bitmap> implements ImageCach
 		this(context , getDefaultLruCacheSize());
 	}
 
+	@SuppressLint("NewApi")
 	public VolleyBitmapLruCache(Context context , int sizeInKiloBytes) {
 		super(sizeInKiloBytes);
 	}
 
+	@SuppressLint("NewApi")
 	@Override
 	protected int sizeOf(String key, Bitmap value) {
 		return value.getRowBytes() * value.getHeight() / 1024;
 	}
 
+	@SuppressLint("NewApi")
 	@Override
 	public Bitmap getBitmap(String url) {
 		return get(url);
